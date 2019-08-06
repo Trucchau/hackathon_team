9562267ba559c003a9a7698e2e8f7a27ec46a7fa
Truc <chaulethanhtruc0401@gmail.com>
2019-08-06 10:48:49 +0700

    Done

--- WP1.py
+++ WP1.py
@@ -50,7 +50,7 @@ def read_3_1(N, n):
     if sohangchuc == 0:
         ans = N[sodautien][0] + " trăm"
     elif sothu2 == 0:
-        ans = N[sodautien][0] + " trăm " + "linh " + N[sothu3][0]
+        ans = N[sodautien][0] + " trăm linh " + N[sothu3][0]
     else:
         ans = N[sodautien][0] + " trăm " + read_2(N, sohangchuc)
     return ans
@@ -87,20 +87,45 @@ def read_thousand(N, n):
         ans = read_3_2(N, sohangtram)
     return ans
 
+def read_thousand_2(N, n):
+    ans = []
+    sohangngan = n // 1000
+    sohangtram = n % 1000
+    if sohangngan < 100:
+        ans = read_3_2(N, sohangngan) + " nghìn " + read_3_2(N, sohangtram)
+    elif sohangngan < 10:
+        ans = read_3_2(N, sohangngan) + "nghìn" + read_3_2(N, sohangtram)
+    elif sohangngan == 0:
+        ans = read_3_2(N, sohangtram)
+    return ans
+
 #Đọc số hàng triệu:
 def read_million(N, n):
     ans = []
     sohangtrieu = n // 1000000
     sohangngan = n % 1000000
     if count_number(sohangtrieu) == 2:
-        ans = read_2(N, sohangtrieu) + " triệu " + read_thousand(N, sohangngan)
+        ans = read_2(N, sohangtrieu) + " triệu " + read_thousand_2(N, sohangngan)
     elif count_number(sohangtrieu) == 3:
-        ans = read_3_1(N, sohangtrieu) + " triệu " + read_thousand(N, sohangngan)
+        ans = read_3_1(N, sohangtrieu) + " triệu " + read_thousand_2(N, sohangngan)
     elif sohangtrieu < 10 and sohangtrieu > 0:
-        ans = N[sohangtrieu][0] + " triệu " + read_thousand(N, sohangngan)
+        ans = N[sohangtrieu][0] + " triệu " + read_thousand_2(N, sohangngan)
     elif sohangtrieu == 0:
         ans = read_thousand(N, sohangngan)
     return ans
+
+def read_million_2(N, n):
+    ans = []
+    sohangtrieu = n // 1000000
+    sohangngan = n % 1000000
+    if sohangtrieu < 100:
+        ans = read_3_2(N, sohangtrieu) + " triệu " + read_thousand_2(N, sohangngan)
+    elif sohangngan < 10:
+        ans = read_3_2(N, sohangtrieu) + " triệu " + read_thousand_2(N, sohangngan)
+    elif sohangngan == 0:
+        ans = read_thousand(N, sohangngan)
+    return ans
+
 
 #Đọc số hàng tỉ:
 def read_billion(N, n):
@@ -108,17 +133,17 @@ def read_billion(N, n):
     sohangty = n // 1000000000
     sohangtrieu = n % 1000000000
     if count_number(sohangty) == 2:
-        ans = read_2(N, sohangty) + " tỉ " + read_million(N, sohangtrieu)
+        ans = read_2(N, sohangty) + " tỉ " + read_million_2(N, sohangtrieu)
     elif count_number(sohangty) == 3:
-        ans = read_3_1(N, sohangty) + " tỉ " + read_million(N, sohangtrieu)
+        ans = read_3_1(N, sohangty) + " tỉ " + read_million_2(N, sohangtrieu)
     elif sohangty < 10 and sohangty > 0:
-        ans = N[sohangty][0] + " tỉ " + read_million(N, sohangtrieu)
+        ans = N[sohangty][0] + " tỉ " + read_million_2(N, sohangtrieu)
     elif sohangty == 0:
         ans = read_million(N, sohangtrieu)
     return ans
 
 #Hàm tổng:
-def read_the_number(n):
+def integer_to_vietnamese_numeral(n):
     if not isinstance(n, int):
         raise TypeError("Not an integer.")
     if n < 0:
@@ -127,10 +152,10 @@ def read_the_number(n):
         raise OverflowError
     if n <= 10:
         result = N[n][0]
-    elif n>10 and n <= 100:
+    elif n > 10 and n <= 100:
         result = read_2(N,n)
     else:
         result = read_billion(N, n)
     return result 
 
-print(read_the_number(n))
+print(integer_to_vietnamese_numeral(n))
